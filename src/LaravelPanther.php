648f21703fe8576b7cbdca592a6e48e50a4e75de
Muhammad Habib <mhabib555@gmail.com>
2022-09-29 16:24:58 +0300

    Update LaravelPanther.php

--- src/LaravelPanther.php
+++ src/LaravelPanther.php
@@ -4,9 +4,5 @@ namespace Mhabib555\LaravelPanther;
 
 class LaravelPanther
 {
-    public function justDoIt() {
-        $response = Http::get('https://inspiration.goprogram.ai/');
 
-        return $response['quote'] . ' -' . $response['author'];
-    }
 }
